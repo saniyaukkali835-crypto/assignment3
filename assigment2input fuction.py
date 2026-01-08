1122e2aeb9e7efeca921ab5398cf36db34fb2ea2
saniyaukkali835-crypto <saniyaukkali835@gmail.com>
2026-01-08 16:09:48 +0530

    Add files via upload

---
+++ assigment2input fuction.py
@@ -0,0 +1,9 @@
+name=input("Enter your name:")
+fav_number=
+
+
+
+c2>>..input("Enter your your favorite number;")
+print("Hello",name,"your favorite number is:",fav_number)
+print(type(name))
+print(type(fav_number))
---
+++ assignment.1vari.py
@@ -0,0 +1,9 @@
+name = "Saniya"
+age = 20
+height = 1.60
+is_student = True
+
+print("Name:", name, "Data Type:", type(name))
+print("Age:", age, "Data Type:", type(age))
+print("Height:", height, "Data Type:", type(height))
+print("Is Student:", is_student, "Data Type:", type(is_student))
