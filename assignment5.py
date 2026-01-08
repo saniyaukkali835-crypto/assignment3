1e2fd8ea1b8a6487297032ba0a8405646dfd96c5
saniyaukkali835-crypto <saniyaukkali835@gmail.com>
2026-01-08 16:16:43 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,12 @@
+a=input("enter first number")
+b=input("enter second number")
+c=int(a)
+d=int(b)
+sum=c+d
+diff=c-d
+prod=c*d
+div=c/d
+print("sum:",sum)
+print("difference:",diff)
+print("product:",prod)
+print("division:",div)
